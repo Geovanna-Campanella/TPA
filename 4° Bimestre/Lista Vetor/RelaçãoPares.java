fed9871333494833c6acc8d6542525f109c60e33
Geovanna Campanella <geovannacampanella08@gmail.com>
2024-11-24 16:28:21 -0300

    Add files via upload

---
+++ 4° Bimestre/Lista Vetor/RelaçãoPares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class RelaçãoPares {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i, j, A[];
+		int TAM = 10;
+		A = new int [TAM];
+		
+		for (j=0; j<10; j++) {
+			System.out.println("Digite o número");
+			A[j] = in.nextInt();
+		
+			for (i=0; i<=A[j]; i=i+2 ) {
+				System.out.println(i);
+			}
+		}
+		
+		in.close();
+	}
+}
