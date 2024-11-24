38e9dc63cb49312d4f523c596b308d8824a37ece
Geovanna Campanella <geovannacampanella08@gmail.com>
2024-11-24 16:34:27 -0300

    Add files via upload

---
+++ 4 Bimestre/ListaVetor/Intersecção.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Intersecção {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i, j, A[], B[], C[];
+		int TAM = 10;
+		A = new int [TAM];
+		B = new int [TAM];
+		C = new int [TAM];
+		
+		for(i=0; i<10; i++) {
+			System.out.println("Digite o " + (i+1) + " número do conjunto A:");
+			A[i] = in.nextInt();
+		}
+			
+		for(j=0; j<10; j++) {
+			System.out.println("Digite o " + (j+1) + " número do conjunto B:");
+			B[j] = in.nextInt();
+			
+			for(i=0; i<10; i++) {
+				if ((B[j]-A[i])==0) {
+					C[i] = B[j];
+				}
+			}
+		}
+		
+		System.out.print("A= ");		
+		for(i=0; i<10; i++) {
+			System.out.print(A[i]+ " ");
+		}
+		System.out.println("");
+		
+		System.out.print("B= ");
+		for(j=0; j<10; j++) {
+			System.out.print(B[j]+ " ");
+		}
+		System.out.println("");
+		
+		System.out.print("C= ");
+		for(i=0; i<10; i++) {
+			if (C[i] > 0)
+			System.out.print(C[i]+ " ");
+		}
+		System.out.println("");
+		
+		in.close();
+		
+	}
+	
+}
---
+++ 4 Bimestre/ListaVetor/RelaçãoDivisores.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class RelaçãoDivisores {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double n, f, A[];
+		int i, j;
+		int TAM = 10;
+		A = new double [TAM];
+	
+		for (j=0; j<10; j++) {
+			System.out.println("Digite o número");
+			A[j] = in.nextInt();
+		
+			System.out.print( "Seus divisores são: " );
+			for (i=0; i<A[j]; i++) {
+				f = A[j]%i;
+				
+				if (f==0){
+					System.out.print( i + " ");
+				} else {
+					
+				}
+			}
+			System.out.println("");
+		}
+		
+		in.close();
+	}
+	
+}
